d4b264a1838a96cd9c9a2f4387c9d43650d71777
alex <alexvitu@yandex.ru>
2023-04-05 03:06:37 +0300

    решение задачи 23

--- Program.cs
+++ Program.cs
@@ -151,19 +151,36 @@
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
 
-System.Console.WriteLine("Введите координату X точки А: ");
-int x1 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите координату Y точки A: ");
-int y1 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите координату Z точки A: ");
-int z1 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите координату X точки B: ");
-int x2 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите координату Y точки B: ");
-int y2 = int.Parse(Console.ReadLine());
-System.Console.WriteLine("Введите координату Z точки B: ");
-int z2 = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Введите координату X точки А: ");
+// int x1 = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Y точки A: ");
+// int y1 = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Z точки A: ");
+// int z1 = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Введите координату X точки B: ");
+// int x2 = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Y точки B: ");
+// int y2 = int.Parse(Console.ReadLine());
+// System.Console.WriteLine("Введите координату Z точки B: ");
+// int z2 = int.Parse(Console.ReadLine());
 
-double rastAB = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
+// double rastAB = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1));
 
-Console.WriteLine($"Расстояние между точками АВ {rastAB}");
+// Console.WriteLine($"Расстояние между точками АВ {rastAB}");
+
+// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+System.Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($" {i*i*i}");
+}
+
